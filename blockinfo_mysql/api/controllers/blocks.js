d846b91d1e7f099f9e88342105f48452829724c7
root <root@localhost.localdomain>
2017-04-19 15:51:42 +0800

    flow clear blocks api working

---
+++ blockinfo_mysql/api/controllers/blocks.js
@@ -0,0 +1,66 @@
+'use strict';
+/*
+ 'use strict' is not required but helpful for turning syntactical errors into true errors in the program flow
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+*/
+
+/*
+ Modules make it possible to import JavaScript files into your application.  Modules are imported
+ using 'require' statements that give you a reference to the module.
+
+  It is a good idea to list the modules that your application depends on in the package.json in the project root
+ */
+var util = require('util');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '47.92.67.21',
+  user     : 'root',
+  password : '123456',
+  database : 'test'
+});
+// connection.connect();
+// connection.query('SELECT * from Blockinfo1', function(err, rows, fields) {
+//   if (err) throw err;
+//   //console.log('The solution is: ', rows[0].solution);
+//   console.log(rows)
+// });
+// connection.end();
+
+/*
+ Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
+
+ For a controller in a127 (which this is) you should export the functions referenced in your Swagger document by name.
+
+ Either:
+  - The HTTP Verb of the corresponding operation (get, put, post, delete, etc)
+  - Or the operationId associated with the operation in your Swagger document
+
+  In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
+  we specify that in the exports of this module that 'hello' maps to the function named 'hello'
+ */
+module.exports = {
+  blocks: blocks
+};
+
+/*
+  Functions in a127 controllers used for operations should take two parameters:
+
+  Param 1: a handle to the request object
+  Param 2: a handle to the response object
+ */
+function blocks(req, res) {
+  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+  // var name = req.swagger.params.name.value || 'stranger';
+  //var hello = util.format('Blocks: 9');
+  //onnection.connect();
+  //onnection.query('SELECT * from Blockinfo1', function(err, rows, fields) {
+  // if (err) throw err;
+  // //console.log('The solution is: ', rows[0].solution);
+  // console.log(rows)
+  // res.json(rows[rows.length-1].blocks);
+
+  //);
+  //onnection.end();
+  // this sends back a JSON response which is a single string
+  res.json("9");
+}
---
+++ blockinfo_mysql/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var SwaggerConnect = require('swagger-connect');
+var app = require('connect')();
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+SwaggerConnect.create(config, function(err, swaggerConnect) {
+  if (err) { throw err; }
+
+  // install middleware
+  swaggerConnect.register(app);
+
+  var port = process.env.PORT || 10010;
+  app.listen(port);
+
+  if (swaggerConnect.runner.swagger.paths['/hello']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  }
+});
