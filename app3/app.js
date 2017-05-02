8255ae30106cc514d9041e4fb5b2f1a1c0f1d3fc
pandong <walker@macdeMacBook-Air-3.local>
2017-05-02 11:14:52 +0800

    daily block table add

--- app3/app.js
+++ app3/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+// var routes = require('./routes/index');
 var app = express();
 
 
@@ -39,7 +39,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
