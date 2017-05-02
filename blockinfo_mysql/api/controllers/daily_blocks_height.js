f34d294f80a86d7d1102057038843b201e9242b1
root <root@localhost.localdomain>
2017-05-02 14:28:49 +0800

    data logic change

--- blockinfo_mysql/api/controllers/daily_blocks_height.js
+++ blockinfo_mysql/api/controllers/daily_blocks_height.js
@@ -42,7 +42,7 @@ var connection = mysql.createConnection(c.mysql_path)
   we specify that in the exports of this module that 'hello' maps to the function named 'hello'
  */
 module.exports = {
-  blocks: blocks
+	daily_blocks_height : daily_blocks_height
 };
 
 /*
@@ -51,22 +51,22 @@ module.exports = {
   Param 1: a handle to the request object
   Param 2: a handle to the response object
  */
-function blocks(req, res) {
+function daily_blocks_height(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
   // var name = req.swagger.params.name.value || 'stranger';
   //var hello = util.format('Blocks: 9');
   //onnection.connect();
-  connection.query('SELECT day , AVG(number) AS avgHeight
-                    FROM (
-                        SELECT LEFT(timestamp,10) AS day,
-                        number AS number
-                        FROM transactions) as daytable',
-                  function(err, rows, fields) {
+  connection.query('SELECT day , AVG(number) AS avgHeight FROM ( SELECT LEFT(timestamp,10) AS day, number AS number FROM transactions) as daytable',
+   function(err, rows, fields) {
       if (err) throw err;
     //console.log('The solution is: ', rows[0].solution);
       console.log(rows[0])
   	  for(var j = 0; j < rows.length; j++) {
-  		     rows[j]= [rows[j].number, rows[j].previous_hash, rows[j].data_hash]
+		  var a = []
+		  for(var x in rows[j]){
+			  a.push(rows[j][x])
+		  }
+  		rows[j]=a
   	  } 
       res.json(rows.join('|'));
 
